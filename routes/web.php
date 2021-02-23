905236ac1bfc0648087090aa59cdf5f002bf97d2
blackcode@DESKTOP-CC0RJLG <aqilblackcode@gmail.com>
2021-02-23 16:53:02 +0700

    templating first

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/about',[AboutController::class, 'index']);
+Route::get('/contact',[ContactController::class, 'index']);
+Route::get('/blog',[BlogController::class, 'index']);
